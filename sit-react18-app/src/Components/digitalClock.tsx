c4581f390a6f3c5becaa26ca312eccd30e38f333
imaheshnagar <112493937+imaheshnagar@users.noreply.github.com>
2024-03-02 11:35:44 +0530

    updated hooks

--- sit-react18-app/src/Components/digitalClock.tsx
+++ sit-react18-app/src/Components/digitalClock.tsx
@@ -22,20 +22,19 @@ const DigitalClock = (): JSX.Element => {
 
 export default DigitalClock;
 
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 
 function MyComponent() {
-    const inputRef = useRef(null);
+  const inputRef = useRef(null);
 
-    function focusInput() {
-        inputRef.current.focus();
-    }
+  function focusInput() {
+    inputRef.current.focus();
+  }
 
-    return (
-        <div>
-            <input ref={inputRef} type="text" />
-            <button onClick={focusInput}>Focus Input</button>
-        </div>
-    );
+  return (
+    <div>
+      <input ref={inputRef} type="text" />
+      <button onClick={focusInput}>Focus Input</button>
+    </div>
+  );
 }
-
