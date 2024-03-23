e525397a985a9ef9cd1e9fb33ac0d9a89cd60fe6
imaheshnagar <112493937+imaheshnagar@users.noreply.github.com>
2024-03-23 20:27:50 +0530

    ALl in one app

--- context-api-demo/src/App.js
+++ context-api-demo/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const products = [
+    { pName: "Apple", price: 20 },
+    { pName: "Banana", price: 3 },
+    { pName: "Orange", price: 10 },
+    { pName: "Grapes", price: 8 },
+  ];
+  const [cart, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  let handleProdChange = (e) => {
+    let pName = e.target.options[e.target.selectedIndex].text;
+    let price = e.target.value;
+    let tCart = [...cart];
+    let obj = { pName, price };
+    let tPrice = parseInt(price);
+    tPrice = total + tPrice;
+    tCart.push(obj);
+
+    setProducts(tCart);
+    setTotal(tPrice);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="customDiv">
+        <h3>Purchase Component</h3>
+        <hr />
+        <select onChange={handleProdChange}>
+          {products.map((product, index) => {
+            return (
+              <option value={product.price} key={index}>
+                {product.pName} - ${product.price}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="customDiv">
+        <h3>Cart Component</h3>
+        <hr />
+        <ul>
+          {cart.map((product, index) => {
+            return <li key={index}>{product.pName} </li>;
+          })}
+        </ul>
+      </div>
+      <div className="customDiv">
+        <h3>Total Component</h3>
+        <hr />
+        <h2>Total : {total}</h2>
+      </div>
     </div>
   );
 }
