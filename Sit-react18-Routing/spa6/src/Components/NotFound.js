cc084b4aae5f3907956a2547a6bc01d4a3b1b0fd
imaheshnagar <112493937+imaheshnagar@users.noreply.github.com>
2024-03-23 19:08:46 +0530

    useNavigate

--- Sit-react18-Routing/spa6/src/Components/NotFound.js
+++ Sit-react18-Routing/spa6/src/Components/NotFound.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <h1>404 Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <button onClick={() => navigate("/")}>Go to Home</button>
     </div>
   );
 };
